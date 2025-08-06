cc3b71d5347391e9dca29c5919b8933ebf4b213a
lucashenz <lucasberselihenz@yahoo.com.br>
2025-08-06 20:30:20 -0300

    Primeiro Commit

---
+++ src/ACMEFish.java
@@ -0,0 +1,13 @@
+public class ACMEFish {
+    //todo software necessita de uma aplicação, neste caso, esta é a classe de aplicação
+
+    private Cardume cardume;
+
+    public ACMEFish() {
+        cardume = new Cardume();
+    }
+
+    public void executar() {
+
+    }
+}
