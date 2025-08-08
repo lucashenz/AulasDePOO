8a940041f227200b7776dfe6d17efe6fa52c520e
lucashenz <lucasberselihenz@yahoo.com.br>
2025-08-08 19:54:10 -0300

    Aula de arraylist

--- src/ACMEFish.java
+++ src/ACMEFish.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class ACMEFish {
     //todo software necessita de uma aplicação, neste caso, esta é a classe de aplicação
 
+    Scanner tc = new Scanner(System.in);
     private Cardume cardume;
 
     public ACMEFish() {
@@ -8,6 +11,12 @@ public class ACMEFish {
     }
 
     public void executar() {
-        System.out.println("Executando fish");
+        System.out.println("Opçao> ");
+        int opcao = tc.nextInt();
+    }
+
+    public void Menu() {
+        System.out.println("Menu de Opcoes \n [1] Cadastrar uma fish");
     }
 }
+
